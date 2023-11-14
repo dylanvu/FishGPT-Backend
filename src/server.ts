19cb3e78c7d4025329294d992e8b498654820959
Dylan Vu <dylanvu9@gmail.com>
2023-11-14 01:03:19 -0800

    working python client

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,26 @@
 import express from "express";
 import http from "http";
 import { Server } from 'socket.io';
+// import mediasoup from "mediasoup";
 
 // express app
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // server for socket.io
 const server = http.createServer(app);
 const io = new Server(server);
 
 // socket.io stuff
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log(`A user has connected! Their ID is ${socket.id}`);
+
+    // context: the python script just turned on, and needs to create an offer with all
+    socket.on('openCVoffer', () => {
+
+    });
+
+
 });
 
 // routes
