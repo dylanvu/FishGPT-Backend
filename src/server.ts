ad1d9c3a174f1b93da163026caa7fa2db1c808cb
Dylan Vu <dylanvu9@gmail.com>
2023-11-13 20:21:45 -0800

    socket.io setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import http from "http";
+import { Server } from 'socket.io';
 
+// express app
 const app = express();
 const port = 3000;
 
+// server for socket.io
+const server = http.createServer(app);
+const io = new Server(server);
+
+// socket.io stuff
+io.on('connection', (socket) => {
+    console.log(`A user has connected! Their ID is ${socket.id}`);
+});
+
+// routes
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
+// actually run the server
+server.listen(port, () => {
     console.log(`FishGPT Backend listening on http://localhost:${port}`);
 });
